d9660ba619092d1d372d93339452df8303cb789a
Wojtek Wydmuch <wojciechwydmuch73@gmail.com>
2020-12-31 22:37:03 +0100

    Added React router and components

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
+import HomePage from './HomePage';
+
 
 export default class App extends Component {
   constructor(props) {
     super(props);
+ 
   }
 
   render() {
-    return <h1> Testing</h1>;
+    return (
+      <div>
+        <HomePage></HomePage>
+      </div>
+      
+      );
   }
 }
 
